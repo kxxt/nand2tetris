807174d0e39ac0eccb1185d7a56fc83ed21f50c3
kxxt <rsworktech@outlook.com>
2022-09-02 22:52:57 +0800

    add 04/mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,48 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// sum=0
+// i=0
+// for i = 0..R1
+//     sum += R0
+// empty loop
+
+@R2
+M=0
+
+@i
+M=0
+
+(LOOP)
+    // if i >= n break
+    @i
+    D=M
+    @R1
+    D=D-M
+    @END
+    D;JGE
+
+    // sum += R0
+    @R0
+    D=M
+    @R2
+    M=M+D
+
+    // i++
+    @i
+    M=M+1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
+
+
+
+
+
+
+

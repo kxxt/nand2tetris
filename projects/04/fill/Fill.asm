6e74815932f9977831c276f6fb97bbfdb35290c1
kxxt <rsworktech@outlook.com>
2022-09-02 23:27:39 +0800

    04/fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,74 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// repreat {
+//   if *KBD == 0
+//      set color white
+//   else
+//      set color black
+//   fill
+// }
+
+// Fill screen with a value in @value
+// screen: 256 * 512 == 8192 * 16
+// The screen memory ends at KBD
+// Procedure:
+// cursor = screen
+// loop
+//   if cursor > screen_end_address
+//     break
+//   *cursor = value
+//   cursor++
+
+
+(LOOP)
+
+// if *KBD == 0
+@KBD
+D=M
+@COLOR_WHITE
+D;JEQ
+
+// color black
+@value
+M=-1
+@FILL
+0;JMP
+
+(COLOR_WHITE)
+@value
+M=0
+
+(FILL)
+    // init cursor to SCREEN
+    @SCREEN
+    D=A
+    @cursor
+    M=D
+
+(FILL_LOOP)
+    @FILL_LOOP
+    // if cursor > screen_end_address
+    //   break
+    @cursor
+    D=M
+    @KBD
+    D=D-A
+    @LOOP
+    D;JGE
+
+    // read value
+    @value
+    D=M
+
+    // write to *cursor
+    @cursor
+    A=M
+    M=D
+
+    // cursor++ 
+    @cursor
+    M=M+1
+    @FILL_LOOP
+    0;JMP

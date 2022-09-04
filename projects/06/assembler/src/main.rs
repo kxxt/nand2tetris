55628d39799a12cbbb9651c643512085f005d19c
kxxt <rsworktech@outlook.com>
2022-09-04 21:15:33 +0800

    feat: assembler cli

--- projects/06/assembler/src/main.rs
+++ projects/06/assembler/src/main.rs
@@ -1,8 +1,34 @@
 mod assembler;
 mod line_translator;
 
+use clap::Parser;
+use std::fs;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author="kxxt", version, about="Hack assembler for nand2tetris course", long_about = None)]
+struct Args {
+    /// input file
+    #[clap(value_parser)]
+    file: String,
+
+    /// output file
+    #[clap(short, long, value_parser)]
+    output: Option<String>,
+}
+
 fn main() {
-    let lines = vec![];
-    let mut _assembler = assembler::Assembler::new(lines);
-    _assembler.compile();
+    let args = Args::parse();
+    let lines = fs::read_to_string(args.file)
+        .expect("Error reading source file!")
+        .lines()
+        .map(str::to_string)
+        .collect();
+    let mut assembler = assembler::Assembler::new(lines);
+    let compiled = assembler.compile();
+    fs::write(
+        args.output.unwrap_or("a.out".to_string()),
+        compiled.join("\n"),
+    )
+    .expect("Failed to write output to file!");
 }

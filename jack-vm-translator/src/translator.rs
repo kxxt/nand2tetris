0b763e12ccf292087d9ff1f85a5f259e778d5738
kxxt <rsworktech@outlook.com>
2022-09-10 21:45:37 +0800

    refactor

--- jack-vm-translator/src/translator.rs
+++ jack-vm-translator/src/translator.rs
@@ -1,14 +1,13 @@
 use crate::command::Command;
 use crate::translation_state::TranslationState;
+use crate::errors::TranslationError;
 
 pub struct Translator;
 
-pub struct TranslationError {
-    message: String,
-}
+
 
 impl Translator {
-    fn translate(commands: impl Iterator<Item = Command>) -> Result<String, TranslationError> {
+    pub fn translate(commands: impl Iterator<Item = Command>) -> Result<String, TranslationError> {
         let mut state = TranslationState::new();
         Ok(commands
             .map(|command| command.to_asm(&mut state))

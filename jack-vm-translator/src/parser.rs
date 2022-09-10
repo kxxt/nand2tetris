dee37839bd5d3d7b993d0b0984bb78d81d7dd567
kxxt <rsworktech@outlook.com>
2022-09-10 21:45:25 +0800

    impl parser

--- jack-vm-translator/src/parser.rs
+++ jack-vm-translator/src/parser.rs
@@ -1,33 +1,22 @@
 use std::str::FromStr;
 
-use crate::command::{Command, ParseCommandError};
+use crate::command::Command;
+use crate::errors::{ParserError, ParseCommandError};
 
 pub struct Parser;
 
-pub struct ParserError {
-    line_number: u32,
-    message: String,
-    inner_error: ParseCommandError,
-}
-
-impl ParserError {
-    fn new(err: ParseCommandError, line_number: u32) -> Self {
-        let message = err.get_message(line_number);
-        Self {
-            line_number,
-            message,
-            inner_error: err,
-        }
-    }
-}
-
 impl Parser {
-    fn parse(source: &str) -> Result<Vec<Command>, ParserError> {
+    pub fn parse(source: &str) -> Result<Vec<Command>, ParserError> {
         source
             .lines()
             .enumerate()
-            .map(|(id, line)| {
-                Command::from_str(line).map_err(|err| ParserError::new(err, id as u32 + 1))
+            .filter_map(|(id, line)| {
+                let result = Command::from_str(line);
+                if let Err(ParseCommandError::NoCommand) = result {
+                    None
+                } else {
+                    Some(result.map_err(|err| ParserError::new(err, id as u32 + 1)))
+                }
             })
             .collect()
     }

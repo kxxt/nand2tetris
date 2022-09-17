9ad0dc9dea0740929b6b6479021eff1aff342857
kxxt <rsworktech@outlook.com>
2022-09-17 21:32:26 +0800

    impl Display for Token

--- jack-compiler/src/token.rs
+++ jack-compiler/src/token.rs
@@ -4,6 +4,12 @@ pub struct Token {
     pub value: String,
 }
 
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} \"{}\"", self.kind, self.value)
+    }
+}
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum TokenKind {
     Keyword,
@@ -11,6 +17,18 @@ pub enum TokenKind {
     Identifier,
     IntegerConstant,
     StringConstant,
+}
+
+impl Display for TokenKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Keyword => write!(f, "keyword"),
+            Self::Symbol => write!(f, "symbol"),
+            Self::Identifier => write!(f, "identifier"),
+            Self::IntegerConstant => write!(f, "integer constant"),
+            Self::StringConstant => write!(f, "string constant"),
+        }
+    }
 }
 
 macro_rules! token {
@@ -21,5 +39,7 @@ macro_rules! token {
         }
     };
 }
+
+use std::fmt::Display;
 
 pub(crate) use token;

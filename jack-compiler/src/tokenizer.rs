c63874091cd64944c00df4d03385f5e40869c441
kxxt <rsworktech@outlook.com>
2022-09-17 13:43:43 +0800

    tokenizer： remove long spaces

--- jack-compiler/src/tokenizer.rs
+++ jack-compiler/src/tokenizer.rs
@@ -15,14 +15,15 @@ pub struct Tokenizer;
 impl Tokenizer {
     pub fn stream<'a>(source: &'a Source) -> TokenStream<'a> {
         lazy_static! {
-            static ref REMOVE_COMMENTS: Regex = Regex::new(r#"/\*(.|\n)*?\*/|//.*"#).unwrap();
+            static ref REMOVE_COMMENTS_AND_LONG_SPACES: Regex =
+                Regex::new(r#"/\*(.|\n)*?\*/|//.*|\s+"#).unwrap();
         }
         let Source {
             content: source,
             name,
         } = source;
         TokenStream {
-            source: REMOVE_COMMENTS.replace_all(&source, ""),
+            source: REMOVE_COMMENTS_AND_LONG_SPACES.replace_all(&source, " "),
             source_name: &name,
         }
     }
@@ -140,86 +141,7 @@ class ParticleSystem {
         let stream = Tokenizer::stream(&source);
         assert_eq!(
             stream.source.to_string(),
-            r##"
-
-class ParticleSystem {
-    field int count;
-    field Array particles;
-    constructor ParticleSystem new() {
-        let count = 0;
-        return this;
-    }
-
-    
-    method void update() {
-        var int i;
-        var Particle cur;
-        let i = 0;
-        while (i < count) {
-            let cur = particles[i]; 
-            do cur.update();
-            let i = i + 1;
-        }
-        return;
-    }
-
-    
-    method void generate() {
-        var int i, xsign, ysign;
-        var Particle p;
-        let i = 0;
-        let xsign = 1;
-        let ysign = 1;
-        while (i < count) {
-            if (Random.rand() > 16383) {
-                let xsign = -xsign;
-            }
-            if (Random.rand() > 16383) {
-                let ysign = -ysign;
-            }
-            let p = Particle.new(Random.randRange(508-3)+3, Random.randRange(252-3)+3, 
-                        xsign * Random.randRange(3), ysign * Random.randRange(3));
-            let particles[i] = p;
-            let i = i + 1;
-        }
-        return;
-    }
-
-    
-    method void run() {
-        var char key;  
-        var boolean exit;
-        do Random.setSeed(2333);
-        do Output.printString("Welcome to particle system!");
-        do Output.println();
-        let count = Keyboard.readInt("How many particles do you want to render?> ");
-        while ((count < 0) | (count = 0)) {
-            let count = Keyboard.readInt("Please enter a positive number!> ");
-        }
-        do Output.printString("Generating particles, please wait...");
-        do Output.println();
-        let particles = Array.new(count);
-        do generate();
-        do Output.printString("Done generation! Press any key to start simulation.");
-        do Output.println();
-        do Keyboard.readChar();
-        do Screen.clearScreen();
-        let exit = false;
-        
-        while (~exit) {
-            let key = Keyboard.keyPressed();
-            do update();
-            if (key = 81)  { let exit = true; }     
-        }
-        return;
-    }
-
-    method void dispose() {
-        do particles.dispose();
-        do Memory.deAlloc(this);
-        return;
-    }
-}"##
+            r##"   class ParticleSystem { field int count; field Array particles; constructor ParticleSystem new() { let count = 0; return this; }   method void update() { var int i; var Particle cur; let i = 0; while (i < count) { let cur = particles[i]; do cur.update(); let i = i + 1; } return; }   method void generate() { var int i, xsign, ysign; var Particle p; let i = 0; let xsign = 1; let ysign = 1; while (i < count) { if (Random.rand() > 16383) { let xsign = -xsign; } if (Random.rand() > 16383) { let ysign = -ysign; } let p = Particle.new(Random.randRange(508-3)+3, Random.randRange(252-3)+3, xsign * Random.randRange(3), ysign * Random.randRange(3)); let particles[i] = p; let i = i + 1; } return; }   method void run() { var char key;   var boolean exit; do Random.setSeed(2333); do Output.printString("Welcome to particle system!"); do Output.println(); let count = Keyboard.readInt("How many particles do you want to render?> "); while ((count < 0) | (count = 0)) { let count = Keyboard.readInt("Please enter a positive number!> "); } do Output.printString("Generating particles, please wait..."); do Output.println(); let particles = Array.new(count); do generate(); do Output.printString("Done generation! Press any key to start simulation."); do Output.println(); do Keyboard.readChar(); do Screen.clearScreen(); let exit = false; while (~exit) { let key = Keyboard.keyPressed(); do update(); if (key = 81) { let exit = true; }   } return; } method void dispose() { do particles.dispose(); do Memory.deAlloc(this); return; } }"##
         );
     }
 }

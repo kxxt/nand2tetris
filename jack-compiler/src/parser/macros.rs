e8862a749ef5bb916842be1489d250a13c90d7bd
kxxt <rsworktech@outlook.com>
2022-09-18 22:27:48 +0800

    ast builder macros

--- jack-compiler/src/parser/macros.rs
+++ jack-compiler/src/parser/macros.rs
@@ -79,20 +79,164 @@ macro_rules! n_ret_type {
 pub(super) use n_ret_type;
 
 macro_rules! n_vars {
-    ($($type:ident $($val:ident),+);+ $(;)?) => {
+    ($($type:ident $($val:ident),+;)*) => {
         vec![
             $(VariableDeclarationNode {
                 r#type: n_type!($type),
                 names: vec![
                     $(stringify!($val).to_string().into()),+
                 ]
-            }),+
+            }),*
         ]
     };
 }
 
 pub(super) use n_vars;
 
-macro_rules! n_subroutine {
-    ($kind:ident $) => {};
+macro_rules! n_expr {
+    ($e:expr) => {
+        ExpressionNode {
+            term: Rc::new(0u16.into()),
+            parts: vec![],
+        }
+    };
 }
+
+pub(super) use n_expr;
+
+macro_rules! n_call {
+    ($name:ident ($e:expr)) => {
+        ExpressionNode {
+            term: Rc::new(SubroutineCallNode {
+                this: None,
+                name: stringify!($name).to_string().into(),
+                arguments: $e,
+            }),
+            parts: vec![],
+        }
+    };
+    ($name:ident.$method:ident ($e:expr)) => {
+        ExpressionNode {
+            term: Rc::new(
+                SubroutineCallNode {
+                    this: Some(stringify!(name).to_string().into()),
+                    name: stringify!($method).to_string().into(),
+                    arguments: $e,
+                }
+                .into(),
+            ),
+            parts: vec![],
+        }
+    };
+}
+
+pub(super) use n_call;
+
+macro_rules! n_string {
+    ($e:expr) => {
+        ExpressionNode {
+            term: Rc::new($e.to_string().into()),
+            parts: vec![],
+        }
+    };
+}
+
+pub(super) use n_string;
+
+macro_rules! n_int {
+    ($e:expr) => {
+        ExpressionNode {
+            term: Rc::new(($e as u16).into()),
+            parts: vec![],
+        }
+    };
+}
+
+pub(super) use n_int;
+
+macro_rules! n_var {
+    ($e:ident) => {
+        ExpressionNode {
+            term: Rc::new(IdentifierNode::from(stringify!($e).to_string()).into()),
+            parts: vec![],
+        }
+    };
+}
+
+pub(super) use n_var;
+
+macro_rules! n_constant {
+    (This) => {
+        ExpressionNode {
+            term: KeywordConstant::This.into(),
+            parts: vec![],
+        }
+    };
+    (Null) => {
+        ExpressionNode {
+            term: KeywordConstant::Null.into(),
+            parts: vec![],
+        }
+    };
+    (True) => {
+        ExpressionNode {
+            term: KeywordConstant::True.into(),
+            parts: vec![],
+        }
+    };
+    (False) => {
+        ExpressionNode {
+            term: KeywordConstant::False.into(),
+            parts: vec![],
+        }
+    };
+}
+
+pub(super) use n_constant;
+
+macro_rules! n_cmd {
+    (Let $name:ident = $e:expr) => {
+        LetNode {
+            name: stringify!($name).to_string().into(),
+            index: None,
+            value: $e,
+        }
+        .into()
+    };
+    (Let $name:ident [$k:expr] = $t:expr ) => {
+        LetNode {
+            name: stringify!($name).to_string().into(),
+            index: $k,
+            value: $t,
+        }
+        .into()
+    };
+    (While ($c:expr) { $b:expr } ) => {
+
+    }
+}
+
+pub(super) use n_cmd;
+
+macro_rules! n_subroutine {
+    ($kind:ident $ret:ident $name:ident ($($type:ident $param:ident),*) {
+        $($body:tt)*
+    }) => {
+        SubroutineDeclarationNode {
+            kind: SubroutineKind::$kind,
+            return_type: n_ret_type!($ret),
+            name: stringify!($name).to_string().into(),
+            parameters: vec![
+                $(ParameterNode {
+                    r#type: n_type!($type),
+                    name: stringify!($param).to_string().into()
+                }),*
+            ],
+            body: SubroutineBody {
+                $($body)*
+            }
+        }
+    };
+}
+
+pub(super) use n_subroutine;

12cd15272177452285f827afd10e2353d535e6fa
kxxt <rsworktech@outlook.com>
2022-09-19 14:12:07 +0800

    add final test for parser (and use pretty_assertion)

--- jack-compiler/src/parser/tests.rs
+++ jack-compiler/src/parser/tests.rs
@@ -5,6 +5,7 @@ use crate::{
     parser::macros::*,
     tokenizer::{Source, Tokenizer},
 };
+use pretty_assertions::assert_eq;
 use std::rc::Rc;
 
 macro_rules! test_parser {
@@ -366,6 +367,323 @@ test_parser!(
 test_parser!(
     test_square_square,
     "Square",
-    r##""##,
-    n_class!("Square", vec![], n_class_vars! {})
+    r##"// This file is part of www.nand2tetris.org
+    // and the book "The Elements of Computing Systems"
+    // by Nisan and Schocken, MIT Press.
+    // File name: projects/10/Square/Square.jack
+    
+    // (same as projects/09/Square/Square.jack)
+    
+    /** Implements a graphical square. */
+    class Square {
+    
+       field int x, y; // screen location of the square's top-left corner
+       field int size; // length of this square, in pixels
+    
+       /** Constructs a new square with a given location and size. */
+       constructor Square new(int Ax, int Ay, int Asize) {
+          let x = Ax;
+          let y = Ay;
+          let size = Asize;
+          do draw();
+          return this;
+       }
+    
+       /** Disposes this square. */
+       method void dispose() {
+          do Memory.deAlloc(this);
+          return;
+       }
+    
+       /** Draws the square on the screen. */
+       method void draw() {
+          do Screen.setColor(true);
+          do Screen.drawRectangle(x, y, x + size, y + size);
+          return;
+       }
+    
+       /** Erases the square from the screen. */
+       method void erase() {
+          do Screen.setColor(false);
+          do Screen.drawRectangle(x, y, x + size, y + size);
+          return;
+       }
+    
+        /** Increments the square size by 2 pixels. */
+       method void incSize() {
+          if (((y + size) < 254) & ((x + size) < 510)) {
+             do erase();
+             let size = size + 2;
+             do draw();
+          }
+          return;
+       }
+    
+       /** Decrements the square size by 2 pixels. */
+       method void decSize() {
+          if (size > 2) {
+             do erase();
+             let size = size - 2;
+             do draw();
+          }
+          return;
+       }
+    
+       /** Moves the square up by 2 pixels. */
+       method void moveUp() {
+          if (y > 1) {
+             do Screen.setColor(false);
+             do Screen.drawRectangle(x, (y + size) - 1, x + size, y + size);
+             let y = y - 2;
+             do Screen.setColor(true);
+             do Screen.drawRectangle(x, y, x + size, y + 1);
+          }
+          return;
+       }
+    
+       /** Moves the square down by 2 pixels. */
+       method void moveDown() {
+          if ((y + size) < 254) {
+             do Screen.setColor(false);
+             do Screen.drawRectangle(x, y, x + size, y + 1);
+             let y = y + 2;
+             do Screen.setColor(true);
+             do Screen.drawRectangle(x, (y + size) - 1, x + size, y + size);
+          }
+          return;
+       }
+    
+       /** Moves the square left by 2 pixels. */
+       method void moveLeft() {
+          if (x > 1) {
+             do Screen.setColor(false);
+             do Screen.drawRectangle((x + size) - 1, y, x + size, y + size);
+             let x = x - 2;
+             do Screen.setColor(true);
+             do Screen.drawRectangle(x, y, x + 1, y + size);
+          }
+          return;
+       }
+    
+       /** Moves the square right by 2 pixels. */
+       method void moveRight() {
+          if ((x + size) < 510) {
+             do Screen.setColor(false);
+             do Screen.drawRectangle(x, y, x + 1, y + size);
+             let x = x + 2;
+             do Screen.setColor(true);
+             do Screen.drawRectangle((x + size) - 1, y, x + size, y + size);
+          }
+          return;
+       }
+    }
+    "##,
+    n_class!(
+        "Square",
+        vec![
+            n_subroutine! {
+                Constructor Square new(int Ax, int Ay, int Asize) {
+                    {let x = n_e!(Ax)},
+                    {let y = n_e!(Ay)},
+                    {let size = n_e!(Asize)},
+                    {do draw()},
+                    {return n_e!(this)}
+                }
+            },
+            n_subroutine! {
+                Method void dispose() {
+                    {do Memory.deAlloc(n_e!(this))},
+                    {return}
+                }
+            },
+            n_subroutine! {
+                Method void draw() {
+                    {do Screen.setColor(n_e!(true))},
+                    {do Screen.drawRectangle(n_e!(x),n_e!(y), n_binop!(n_t!(x), Plus, n_t!(size)), n_binop!(n_t!(y), Plus, n_t!(size)))},
+                    {return}
+                }
+            },
+            n_subroutine! {
+                Method void erase() {
+                    {do Screen.setColor(n_e!(false))},
+                    {do Screen.drawRectangle(n_e!(x),n_e!(y), n_binop!(n_t!(x), Plus, n_t!(size)), n_binop!(n_t!(y), Plus, n_t!(size)))},
+                    {return}
+                }
+            },
+            n_subroutine! {
+                Method void incSize() {
+                    {if (
+                        n_binop!(
+                            TermNode::Parentheses(Rc::new(n_binop!(
+                                TermNode::Parentheses(
+                                    Rc::new(
+                                        n_binop!(n_t!(y), Plus, n_t!(size))
+                                    )
+                                ),LessThan,n_int_t!(254)
+                            ))),
+                            And,
+                            TermNode::Parentheses(Rc::new(n_binop!(
+                                TermNode::Parentheses(
+                                    Rc::new(
+                                        n_binop!(n_t!(x), Plus, n_t!(size))
+                                    )
+                                ),LessThan,n_int_t!(510)
+                            )))
+                        )
+                    ) {
+                        {do erase()},
+                        {let size = n_binop!(n_t!(size), Plus, n_int_t!(2))},
+                        {do draw()}
+                    }},
+                    {return}
+                }
+            },
+            n_subroutine! {
+                Method void decSize() {
+                    {if (
+                        n_binop!(
+                            n_t!(size), GreaterThan, n_int_t!(2)
+                        )
+                    ) {
+                        {do erase()},
+                        {let size = n_binop!(n_t!(size), Minus, n_int_t!(2))},
+                        {do draw()}
+                    }},
+                    {return}
+                }
+            },
+            n_subroutine! {
+                Method void moveUp() {
+                    {if (n_binop!(
+                        n_t!(y), GreaterThan, n_int_t!(1)
+                    )) {
+                        {do Screen.setColor(n_e!(false))},
+                        {do Screen.drawRectangle(
+                            n_e!(x),
+                            n_binop!(
+                                TermNode::Parentheses(
+                                    Rc::new(n_binop!(
+                                        n_t!(y),Plus,n_t!(size)
+                                    ))
+                                ),
+                                Minus,
+                                n_int_t!(1)
+                            ),
+                            n_binop!(n_t!(x), Plus, n_t!(size)),
+                            n_binop!(n_t!(y), Plus, n_t!(size))
+                        )},
+                        {let y = n_binop!(n_t!(y), Minus, n_int_t!(2))},
+                        {do Screen.setColor(n_e!(true))},
+                        {do Screen.drawRectangle(n_e!(x), n_e!(y),
+                            n_binop!(n_t!(x),Plus,n_t!(size)),
+                            n_binop!(n_t!(y),Plus,n_int_t!(1)),
+                        )}
+                    }},
+                    {return}
+                }
+            },
+            n_subroutine! {
+                Method void moveDown() {
+                    {if (n_binop!(
+                        TermNode::Parentheses(
+                            Rc::new(n_binop!(
+                                n_t!(y), Plus, n_t!(size)
+                            ))
+                        ), LessThan, n_int_t!(254)
+                    )) {
+                        {do Screen.setColor(n_e!(false))},
+                        {do Screen.drawRectangle(n_e!(x), n_e!(y),
+                            n_binop!(n_t!(x),Plus,n_t!(size)),
+                            n_binop!(n_t!(y),Plus,n_int_t!(1)),
+                        )},
+                        {let y = n_binop!(n_t!(y), Plus, n_int_t!(2))},
+                        {do Screen.setColor(n_e!(true))},
+                        {do Screen.drawRectangle(
+                            n_e!(x),
+                            n_binop!(
+                                TermNode::Parentheses(
+                                    Rc::new(n_binop!(
+                                        n_t!(y), Plus, n_t!(size)
+                                    ))
+                                ),
+                                Minus,
+                                n_int_t!(1)
+                            ),
+                            n_binop!(n_t!(x), Plus, n_t!(size)),
+                            n_binop!(n_t!(y), Plus, n_t!(size))
+                        )}
+                    }},
+                    {return}
+                }
+            },
+            n_subroutine! {
+                Method void moveLeft() {
+                    {if (n_binop!(
+                        n_t!(x), GreaterThan, n_int_t!(1)
+                    )) {
+                        {do Screen.setColor(n_e!(false))},
+                        {do Screen.drawRectangle(
+                            n_binop!(
+                                TermNode::Parentheses(
+                                    Rc::new(n_binop!(
+                                        n_t!(x),Plus,n_t!(size)
+                                    ))
+                                ),
+                                Minus,
+                                n_int_t!(1)
+                            ),
+                            n_e!(y),
+                            n_binop!(n_t!(x), Plus, n_t!(size)),
+                            n_binop!(n_t!(y), Plus, n_t!(size))
+                        )},
+                        {let x = n_binop!(n_t!(x), Minus, n_int_t!(2))},
+                        {do Screen.setColor(n_e!(true))},
+                        {do Screen.drawRectangle(n_e!(x), n_e!(y),
+                            n_binop!(n_t!(x),Plus,n_int_t!(1)),
+                            n_binop!(n_t!(y),Plus,n_t!(size)),
+                        )}
+                    }},
+                    {return}
+                }
+            },
+            n_subroutine! {
+                Method void moveRight() {
+                    {if (n_binop!(
+                        TermNode::Parentheses(
+                            Rc::new(n_binop!(
+                                n_t!(x), Plus, n_t!(size)
+                            ))
+                        ), LessThan, n_int_t!(510)
+                    )) {
+                        {do Screen.setColor(n_e!(false))},
+                        {do Screen.drawRectangle(n_e!(x), n_e!(y),
+                            n_binop!(n_t!(x),Plus,n_int_t!(1)),
+                            n_binop!(n_t!(y),Plus,n_t!(size)),
+                        )},
+                        {let x = n_binop!(n_t!(x), Plus, n_int_t!(2))},
+                        {do Screen.setColor(n_e!(true))},
+                        {do Screen.drawRectangle(
+                            n_binop!(
+                                TermNode::Parentheses(
+                                    Rc::new(n_binop!(
+                                        n_t!(x), Plus, n_t!(size)
+                                    ))
+                                ),
+                                Minus,
+                                n_int_t!(1)
+                            ),
+                            n_e!(y),
+                            n_binop!(n_t!(x), Plus, n_t!(size)),
+                            n_binop!(n_t!(y), Plus, n_t!(size))
+                        )}
+                    }},
+                    {return}
+                }
+            },
+        ],
+        n_class_vars! {
+            Field int x, y;
+            Field int size;
+        }
+    )
 );

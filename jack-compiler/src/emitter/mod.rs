719ae7e8d20c7fad3cb21d6d24786208be15933e
kxxt <rsworktech@outlook.com>
2022-09-21 11:46:00 +0800

    init emitter mod

---
+++ jack-compiler/src/emitter/mod.rs
@@ -0,0 +1,34 @@
+use crate::{ast::*, compiler::VMCode};
+
+mod classes;
+
+pub struct Emitter {
+    ast: AST,
+}
+
+impl Emitter {
+    pub fn new(ast: AST) -> Self {
+        Self { ast }
+    }
+
+    pub fn emit(&mut self) -> VMCode {
+        todo!()
+    }
+
+    fn emit_subroutine(&mut self, routine: SubroutineDeclarationNode) -> VMCode {
+        todo!()
+    }
+
+    fn emit_expr(&mut self, routine: ExpressionNode) -> VMCode {
+        todo!()
+    }
+
+    fn emit_class_var(&mut self, class_var: ClassVariableDeclarationNode) -> VMCode {
+        todo!()
+    }
+
+    fn emit_statement(&mut self, statement: StatementNode) -> VMCode {
+        todo!()
+    }
+
+}

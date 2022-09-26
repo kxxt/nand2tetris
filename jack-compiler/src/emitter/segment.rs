d9554b7bf3ac442a8603ad29c779de52bb7460ff
kxxt <rsworktech@outlook.com>
2022-09-26 09:34:00 +0800

    impl Display for Segment

--- jack-compiler/src/emitter/segment.rs
+++ jack-compiler/src/emitter/segment.rs
@@ -1,6 +1,9 @@
 use anyhow::anyhow;
 use phf::phf_map;
-use std::str::FromStr;
+use std::{
+    fmt::{write, Display},
+    str::FromStr,
+};
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub enum Segment {
@@ -12,6 +15,21 @@ pub enum Segment {
     Temp,
     Constant,
     Pointer,
+}
+
+impl Display for Segment {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Segment::Static => write!(f, "static"),
+            Segment::Local => write!(f, "local"),
+            Segment::Argument => write!(f, "argument"),
+            Segment::This => write!(f, "this"),
+            Segment::That => write!(f, "that"),
+            Segment::Temp => write!(f, "temp"),
+            Segment::Constant => write!(f, "constant"),
+            Segment::Pointer => write!(f, "pointer"),
+        }
+    }
 }
 
 static FROMSTR_MAP: phf::Map<&'static str, Segment> = phf_map! {

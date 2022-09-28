73c0db357b649a3f28522efb80b45157219653d9
kxxt <rsworktech@outlook.com>
2022-09-28 09:15:10 +0800

    also return module name in Compiler

--- jack-compiler/src/compiler.rs
+++ jack-compiler/src/compiler.rs
@@ -1,5 +1,3 @@
-use std::error::Error;
-
 use crate::{
     emitter::Emitter,
     parser::Parser,
@@ -11,9 +9,9 @@ pub struct Compiler;
 pub type VMCode = String;
 
 impl Compiler {
-    pub fn compile(source: Source) -> Result<VMCode, Box<dyn Error>> {
+    pub fn compile(source: Source) -> Result<(VMCode, String), anyhow::Error> {
         let token_stream = Tokenizer::stream(&source);
         let ast = Parser::new(token_stream, source.name.to_string()).parse()?;
-        Ok(Emitter::new().emit(&ast)?)
+        Ok((Emitter::new().emit(&ast)?, source.name))
     }
 }
